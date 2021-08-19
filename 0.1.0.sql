cc6d4badb47c57aae5a581d3a03fcd6d507cd0df
censorfriendly <jimmd111@gmail.com>
2021-08-19 07:59:27 -0500

    adding item sql and warning

---
+++ 0.1.0.sql
@@ -0,0 +1,4 @@
+-- Example this will need to be modified and is not ready to run out of the box. This is used to allow the item to trigger opening the racing app. If you do not plan on using the item and command only do not add
+
+INSERT INTO `es_extended`.`items` (`name`, `label`, `weight`, `rare`, `can_remove`)
+VALUES ('racetablet', 'Racing Tablet', '2.0', '0', '1')
